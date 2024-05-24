546473ad9fc8e9823314a582ee7a72fbe308b5a9
Piers Karsenbarg <piers@pulumi.com>
2024-05-24 10:44:34 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	p "github.com/pulumi/pulumi-go-provider"
+	"github.com/pulumi/pulumi-go-provider/infer"
+	"github.com/pulumi/pulumi-go-provider/middleware/schema"
+	dotnetgen "github.com/pulumi/pulumi/pkg/v3/codegen/dotnet"
+	gogen "github.com/pulumi/pulumi/pkg/v3/codegen/go"
+	nodejsgen "github.com/pulumi/pulumi/pkg/v3/codegen/nodejs"
+	pythongen "github.com/pulumi/pulumi/pkg/v3/codegen/python"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
+)
+
+func main() {
+	err := p.RunProvider("cloudflare-native", "0.1.0", provider())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func provider() p.Provider {
+	return infer.Provider(infer.Options{
+		Metadata: schema.Metadata{
+			DisplayName: "cloudflare-native",
+			Description: "Cloudflare Native Pulumi provider",
+			LanguageMap: map[string]any{
+				"go": gogen.GoPackageInfo{
+					ImportBasePath: "github.com/pierskarsenbarg/pulumi-cloudflare-native/sdk/go/cloudflare-native",
+				},
+				"nodejs": nodejsgen.NodePackageInfo{
+					PackageName: "@pierskarsenbarg/cloudflare-native",
+					Dependencies: map[string]string{
+						"@pulumi/pulumi": "^3.0.0",
+					},
+					DevDependencies: map[string]string{
+						"@types/node": "^10.0.0", // so we can access strongly typed node definitions.
+						"@types/mime": "^2.0.0",
+					},
+				},
+				"csharp": dotnetgen.CSharpPackageInfo{
+					RootNamespace: "PiersKarsenbarg",
+					PackageReferences: map[string]string{
+						"Pulumi": "3.*",
+					},
+				},
+				"python": pythongen.PackageInfo{
+					Requires: map[string]string{
+						"pulumi": ">=3.0.0,<4.0.0",
+					},
+					PackageName: "pierskarsenbarg_pulumi_cloudflare_native",
+				},
+			},
+			PluginDownloadURL: "github://api.github.com/pierskarsenbarg/pulumi-cloudflare-native",
+			Publisher:         "Piers Karsenbarg",
+		},
+		Resources: []infer.InferredResource{
+			// infer.Resource[*pkg.Account, pkg.AccountArgs, pkg.AccountState](),
+		},
+		ModuleMap: map[tokens.ModuleName]tokens.ModuleName{
+			"pkg": "index", // required because the folder with everything in is "pkg"
+		},
+		Functions: []infer.InferredFunction{
+			// infer.Function[*pkg.GetAccount, pkg.GetAccountArgs, pkg.AccountState](),
+		},
+		//Config: infer.Config[*pkg.Config](),
+	})
+}
